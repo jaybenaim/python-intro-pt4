c435487d7e04f453d72a3808c345c91d3909279d
jaybenaim <benaimjacob@gmail.com>
2019-07-11 16:23:44 -0400

    added exercise 1 to exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,32 @@
+fav_colors = ["blue", "red", "black", "green"]
+
+siblings_age = [24, 20, 25, 40]
+
+coin_toss = ["heads", "tails", "tails", "heads", "tails"]
+
+fav_artists = ["RHCP", "Azuna", "Romeo Santos"]
+
+fav_words = {
+    "abject": "expeirenced or present to the maximum degree",
+    "direful": "extremely bad or dreadful",
+    "furtive": "attempting to avoid notice or attention, typically because of guilt",
+}
+
+fav_movies = {"hulk": 2003, "juice": 1992, "city_of_god": 2002}
+
+fav_cities = {"san_juan": 347052, "st_maarten": 41109, "st_thomas": 41813}
+
+friends = {"me": 24, "eden": 25, "steph": 20}
+
+# get the last item in the list 
+print(fav_colors[-1])
+
+# add city to fav_cities 
+fav_cities['toronto'] = 2930000
+
+print(fav_cities)
+
+# print year of one movie 
+print(fav_movies['hulk'])
+
+
