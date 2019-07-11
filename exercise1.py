1442ae473dbf59d220bc8547b229c449c34b3a79
jaybenaim <benaimjacob@gmail.com>
2019-07-11 16:03:57 -0400

    added exercise1.py

---
+++ exercise1.py
@@ -0,0 +1,40 @@
+fav_colours = ['blue', 'red', 'black', 'green']
+
+siblings_age = [24, 20, 25, 40]
+
+coin_toss = ['heads', 'tails', 'tails', 'heads', 'tails']
+
+fav_artists = ['RHCP', 'Azuna', 'Romeo Santos']
+
+fav_words = {
+    'abject': 'expeirenced or present to the maximum degree', 
+    'direful': 'extremely bad or dreadful', 
+    'furtive': 'attempting to avoid notice or attention, typically because of guilt'
+}
+
+fav_movies = { 
+    'hulk': 2003, 
+    'juice': 1992, 
+    'city_of_god': 2002
+}
+
+fav_cities = { 
+    'san_juan': 347052,
+    'st_maarten': 41109, 
+    'st_thomas': 41813
+}
+
+friends = { 
+    'me': 24, 
+    'eden': 25, 
+    'steph': 20
+}
+
+print(coin_toss) 
+print(fav_colours[0])
+siblings_age.sort()
+print(siblings_age)
+siblings_age.append(0)
+print(siblings_age)
+print(fav_movies['hulk'])
+
