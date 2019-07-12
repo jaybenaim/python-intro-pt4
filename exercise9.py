6c7aa1d3d105eae1c5f7274b3fcf023741aef1ca
jaybenaim <benaimjacob@gmail.com>
2019-07-12 13:59:45 -0400

    exercise 9.1 done without a function

--- exercise9.py
+++ exercise9.py
@@ -2,9 +2,19 @@
 # ------------------------------------------------------------------------------ # 
                             # Exercise 9.1
 # ------------------------------------------------------------------------------ # 
-
-grocery_list = ["steak", "eggs", "milk", 'butter' ]
-
+# your_list = input("what is your list? Place in list format ['gas', 'ect' ] ")
+grocery_list = ["steak", "eggs", "milk", 'butter' ] 
+# def print_item(list_name):
+    # new_list = [] 
 for item in grocery_list: 
-    print("* " + item)
+    # item =  print(f"* {item}") 
+    # # item = print(item)
+    # new_list.append(item)
+    # x = "* {}".format(item)
+    print("* {}".format(item))
 
+    # new_list.append(item)
+# return item 
+    # return new_list
+
+# print(print_item(grocery_list))
