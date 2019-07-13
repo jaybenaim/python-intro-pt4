43366057bd58a0cd787681f4a0250b8496df6a61
jaybenaim <benaimjacob@gmail.com>
2019-07-12 22:22:52 -0400

    exercise 10.5

--- exercise10.py
+++ exercise10.py
@@ -1,3 +1,4 @@
+line_break = "-----------------------------------------------------------------------------------"
 students = {
   'cohort1': 34,
   'cohort2': 42,
@@ -9,4 +10,40 @@ def display_name_and_num(dict_name):
     for student, number in dict_name.items(): 
         print(f'{student}: {number} students')
 
-display_name_and_num(students)
+# display_name_and_num(students)
+# ----------------------------------------------------------------------------------- 
+# 10.3 - 
+# ----------------------------------------------------------------------------------- e
+
+# add 'cohort4' : 43 to students 
+students['cohort4'] = 43
+# display_name_and_num(students)
+# ----------------------------------------------------------------------------------- 
+# 10.4 - 
+# ----------------------------------------------------------------------------------- e
+# display names from students dictionary 
+def display_name(dict_name): 
+    for name in dict_name: 
+        print(name)
+
+# output all the cohort names 
+display_name(students.keys())
+
+# ----------------------------------------------------------------------------------- 
+# 10.5 - 
+# ----------------------------------------------------------------------------------- e
+
+def expand_class(class_list): 
+   # for every student number in class list 
+    for student, number in class_list.items(): 
+    # increase the student number by 5%
+        number += number * 0.05
+        print(student, number) 
+
+# display results 
+expand_class(students)
+# ----------------------------------------------------------------------------------- 
+# 10.6 - 
+# ----------------------------------------------------------------------------------- e
+
+# Delete the 2nd cohort and redisplay the dictionary.
